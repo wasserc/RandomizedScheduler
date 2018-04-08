e2c20edb3247f9998cfb6169d4bbb9b032ba712b
Unknown <wasserc@gmail.com>
2018-04-07 19:18:06 -0500

    cleanup

--- _Tests/DoodleXLSImportTest.py
+++ _Tests/DoodleXLSImportTest.py
@@ -17,11 +17,14 @@ class DoodleXLSImportTest(unittest.TestCase):
         datesRowNumber = 5
         importer = DoodleXLSImport(excelFile, datesRowNumber)
         dict_list = importer.get_dict_from_xls()
-        self.assertEqual(len(dict_list), 4)
+        self.assertEqual(len(dict_list), 7)
         self.assertDictEqual(dict_list[0], {'Name': 'Jim', 'Sat 7': 'OK', 'Mon 9': 'OK', 'Thu 12': '', 'Sat 14': ''})
         self.assertDictEqual(dict_list[1], {'Name': 'Bill', 'Sat 7': 'OK', 'Mon 9': 'OK', 'Thu 12': '', 'Sat 14': 'OK'})
         self.assertDictEqual(dict_list[2], {'Name': 'Fred', 'Sat 7': 'OK', 'Mon 9': 'OK', 'Thu 12': 'OK', 'Sat 14': 'OK'})
         self.assertDictEqual(dict_list[3], {'Name': 'Joe', 'Sat 7': 'OK', 'Mon 9': '', 'Thu 12': 'OK', 'Sat 14': 'OK'})
+        self.assertDictEqual(dict_list[4], {'Name': 'John', 'Sat 7': '', 'Mon 9': 'OK', 'Thu 12': '', 'Sat 14': ''})
+        self.assertDictEqual(dict_list[5], {'Name': 'Sarah', 'Sat 7': 'OK', 'Mon 9': '', 'Thu 12': 'OK', 'Sat 14': ''})
+        self.assertDictEqual(dict_list[6], {'Name': 'Carol', 'Sat 7': '', 'Mon 9': 'OK', 'Thu 12': 'OK', 'Sat 14': 'OK'})
 
     def testDictionaryIsCreatedWhenDifferentWhitespaceIsPresent(self):
         excelFile = "XLSFiles\\SimpleDoodle2.xls"
--- src/CSVToDict.py
+++ src/CSVToDict.py
@@ -4,9 +4,6 @@ class CSVToDict(object):
     def __init__(self, file_path):
         self.file_path = file_path
         self.__retrieve_dictionary(file_path)
-#         workbook = xlrd.open_workbook(file_path)
-#         self.poll_sheet = workbook.sheet_by_name('Poll')
-#         self.__get_column_names(row_number_for_dates_in_xls_file - 1)
         
     def get_dictionary(self):
         return self.dictionary_list
