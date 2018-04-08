428d08f20acaa3b2b8df32e6e1ddc6a64aac84a6
Unknown <wasserc@gmail.com>
2018-04-07 22:49:45 -0500

    Add Data Handler

---
+++ _Tests/DictDataHandlerTest.py
@@ -0,0 +1,33 @@
+import unittest
+from DictDataHandler import DictDataHandler
+from DoodleXLSImport import DoodleXLSImport
+from CSVToDict import CSVToDict
+
+class DictDataHandlerTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.availability = DoodleXLSImport("XLSFiles\\SimpleDoodle.xls", 5)
+        self.mentor_list = CSVToDict("XLSFiles\\SampleMentorsList.csv")
+        self.assignment_weights = CSVToDict("XLSFiles\\SampleMentorWeights.csv")
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testGetListMentorListReturnsDictionaryListWithAvailabilityAndAbilitiesAndWeights(self):
+        data_handler = DictDataHandler(self.availability, self.mentor_list, self.assignment_weights)
+        
+        dict_list = data_handler.get_full_dictionary_list()
+        self.assertEqual(len(dict_list), 7)
+        self.assertDictEqual(dict_list['Bill'], {'Name': 'Bill', 'Sat 7': 'OK', 'Mon 9': 'OK', 'Thu 12': '', 'Sat 14': 'OK', 'Leadership': 'Y', 'Gender': 'M', 'RouteLead': 'Y', 'Count': '5'})
+        self.assertDictEqual(dict_list['Jim'], {'Name': 'Jim', 'Sat 7': 'OK', 'Mon 9': 'OK', 'Thu 12': '', 'Sat 14': '', 'Leadership': 'N', 'Gender': 'M', 'RouteLead': 'N', 'Count': '10'})
+        self.assertDictEqual(dict_list['John'], {'Name': 'John', 'Sat 7': '', 'Mon 9': 'OK', 'Thu 12': '', 'Sat 14': '', 'Leadership': 'Y', 'Gender': 'M', 'RouteLead': 'N', 'Count': '7'})
+        self.assertDictEqual(dict_list['Joe'], {'Name': 'Joe', 'Sat 7': 'OK', 'Mon 9': '', 'Thu 12': 'OK', 'Sat 14': 'OK', 'Leadership': 'N', 'Gender': 'M', 'RouteLead': 'Y', 'Count': '3'})
+        self.assertDictEqual(dict_list['Fred'], {'Name': 'Fred', 'Sat 7': 'OK', 'Mon 9': 'OK', 'Thu 12': 'OK', 'Sat 14': 'OK', 'Leadership': 'N', 'Gender': 'M', 'RouteLead': 'N', 'Count': '8'})
+        self.assertDictEqual(dict_list['Sarah'], {'Name': 'Sarah', 'Sat 7': 'OK', 'Mon 9': '', 'Thu 12': 'OK', 'Sat 14': '', 'Leadership': 'N', 'Gender': 'F', 'RouteLead': 'N', 'Count': '12'})
+        self.assertDictEqual(dict_list['Carol'], {'Name': 'Carol', 'Sat 7': '', 'Mon 9': 'OK', 'Thu 12': 'OK', 'Sat 14': 'OK', 'Leadership': 'Y', 'Gender': 'F', 'RouteLead': 'Y', 'Count': '2'})
+ 
+ 
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
---
+++ src/DictDataHandler.py
@@ -0,0 +1,56 @@
+from CSVToDict import CSVToDict
+from DoodleXLSImport import DoodleXLSImport
+
+from collections import defaultdict
+
+class DictDataHandler(object):
+    def __init__(self, availability, mentor_list, assignment_weights):
+        self.availability = availability
+        self.mentor_list = mentor_list
+        self.assignment_weights = assignment_weights
+        
+        self.initialize_full_dictionary()        
+        self.add_to_full_dictionary(self.mentor_list.get_dictionary())
+        self.add_to_full_dictionary(self.assignment_weights.get_dictionary())
+        
+    def get_full_dictionary_list(self):
+        return self.full_dictionary
+    
+    def initialize_full_dictionary(self):
+        key = 'Name'
+        self.full_dictionary = defaultdict(dict)
+        for elem in self.availability.get_dict_from_xls():
+            self.full_dictionary[elem[key]].update(elem)
+            
+    def add_to_full_dictionary(self, dictionary_list):
+        key = 'Name'
+        for elem in dictionary_list:
+            self.full_dictionary[elem[key]].update(elem)      
+    
+    def _join_dictionaries_on_name(self, dictionaryList1, dictionaryList2, joinType):
+        key = 'Name'
+        d = defaultdict(dict)
+        for elem in dictionaryList1:
+            found = False
+            d[elem[key]].update(elem)
+            for ability in dictionaryList2:
+                if elem[key] == ability[key]:
+                    
+                    d[elem[key]].update(ability)
+                    found = True
+                    break
+            if not found:
+                print(elem[key], " was not found in ", joinType, " key = ", key)
+                print(elem)
+        return d
+    
+#     def get_column_name
+#     
+#     def __retrieve_dictionary(self, file_path):
+#         self.dictionary_list = list()
+#         with open(file_path) as f:
+#             csv_f = csv.DictReader(f)
+#             for thevalue in csv_f:
+#                 self.dictionary_list.append(thevalue)
+#         f.close()
+#         self.field_Names = csv_f.fieldnames
